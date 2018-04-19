ea2395c74b2601fe676ce3ae6516b8d248821317
totokuyu <tolgakuyucuk@gmail.com>
2018-04-19 02:04:48 +0200

    Çoklu link demo

--- scrapyproject/spiders/sneakerslinkler.py
+++ scrapyproject/spiders/sneakerslinkler.py
@@ -1,4 +1,4 @@
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import scrapy
 from scrapy import Selector
 import sqlite3
@@ -7,29 +7,33 @@ conn = sqlite3.connect('linkler.db')
 c = conn.cursor()
 
 
-
 class MySpider(scrapy.Spider):
     name = "sneakerslinkler"
     start_urls = [
-        'https://www.sportscheck.com/schuhe/herren/'
+        'https://www.sportscheck.com/schuhe/herren/',
+        'https://www.sportscheck.com/s/sneakers/'
     ]
+
+    linksayisi = len(start_urls)
+    print(linksayisi)
 
     def parse(self, response):
 
-        with open("sneakerslinkler.txt", "a", encoding = "utf-8") as file:
+        with open("sneakerslinkler.txt", "a", encoding="utf-8") as file:
 
-            for hey in response.xpath("/html"):
-                liste = hey.xpath("//body/div[4]/main/div[4]/div/div/div/a/@href").extract()
-                elemansayisi=len(liste)+1
-
+            for hey in response.xpath("//html/body"):
+                liste = hey.xpath("//main/div[4]/div/div/div[3]/div/div[1]/a/@href").extract()
+                elemansayisi = (len(liste) + 1) * 2
             dizi = [1]
             links = dizi * (elemansayisi)
 
-            for title in response.xpath("/html"):
+            for title in response.xpath("//html/body"):
 
-                for x in range (1,elemansayisi):
-                    links[x] = title.xpath("//body/div[4]/main/div[4]/div[" +str(x)+ "]/div/div/a/@href").extract()[0]
-                    file.write("________________________________________________________________________________________________________________________________")
+                for x in range(1, elemansayisi):
+                    links[x] = title.xpath("//main/div[4]/div[" + str(x) + "]/div/div[3]/div/div[1]/a/@href").extract()[
+                        0]
+                    file.write(
+                        "________________________________________________________________________________________________________________________________")
                     file.write("\n" + "link" + str(x) + ": " + str(links[x]) + "\n")
 
                     c.execute("CREATE TABLE IF NOT EXISTS stuffToPlot(link TEXT)")
